2c4c8af83c99043c491fdf6dc0301eb5ebb05357
Suprakit Sujariyakul <suprakitsujariyakul@gmail.com>
2023-01-23 15:58:08 +0700

    [02-setup] - styling css, setup background

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
-import { DatePicker, Button } from 'antd';
+import { DatePicker, Button, Image } from 'antd';
+import pokemonLogoSrc from './assets/images/pokedex.png'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <div>React App</div>
+      <Image width={200} src={pokemonLogoSrc} />
       <DatePicker />
       <Button type="primary">Click me</Button>
     </>
