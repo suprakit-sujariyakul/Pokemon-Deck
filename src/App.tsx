1215e361eb4a983a17b81517c5b9adb764fae1b0
Suprakit Sujariyakul <suprakitsujariyakul@gmail.com>
2023-01-23 08:49:45 +0700

    [02-setup] - install UI dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import './App.css'
+import { DatePicker, Button } from 'antd';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      <DatePicker />
+      <Button type="primary">Click me</Button>
     </>
   )
 }
