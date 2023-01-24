a4cc381885b5299708daee879bfa4842c4dd0170
Suprakit Sujariyakul <suprakitsujariyakul@gmail.com>
2023-01-24 09:51:51 +0700

    [02-setup] - setup alias path and proflie folder structure with atomic system

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import { DatePicker, Button, Image } from 'antd';
+import { Image } from 'antd'
 import pokemonLogoSrc from './assets/images/pokedex.png'
+import { Button } from '@atomic'
+import { log } from '@utilities'
+
 
 function App() {
   const [count, setCount] = useState(0)
+
+  log("hello");
 
   return (
     <>
       <div>React App</div>
       <Image width={200} src={pokemonLogoSrc} />
-      <DatePicker />
       <Button type="primary">Click me</Button>
     </>
   )
